5ec399fb908f6aab4af046ee9db21b87b28cea8a
Jordan <willsy2k6@hotmail.co.uk>
2015-03-27 20:19:49 +0000

    Update

--- app/src/main/java/com/mygymcompanion/mygymcompanion/HomeActivity.java
+++ app/src/main/java/com/mygymcompanion/mygymcompanion/HomeActivity.java
@@ -1,39 +1,99 @@
 package com.mygymcompanion.mygymcompanion;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-
+/*
+ * Created by Jordan on 22/03/2015.
+ */
 public class HomeActivity extends ActionBarActivity {
+
+    private Button buttonReset, buttonStats, buttonWorkout, buttonBMI, buttonMachines, buttonEditWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        final SharedPreferences.Editor editor = settings.edit();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Boolean firstTime = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("FirstLaunch", true);
+        if(firstTime) {
+            Intent myIntent = new Intent(HomeActivity.this, FirstActivity.class);
+            HomeActivity.this.startActivity(myIntent);
         }
 
-        return super.onOptionsItemSelected(item);
+        //reset app data
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                editor.clear();
+                editor.commit();
+
+                Toast.makeText(HomeActivity.this, "App data reset", Toast.LENGTH_LONG).show();
+
+                Intent myIntent = new Intent(HomeActivity.this, FirstActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+        //do when button stats pressed
+        buttonStats = (Button) findViewById(R.id.buttonStats);
+        buttonStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(HomeActivity.this, StatsActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+
+        //do when button machines pressed
+        buttonMachines = (Button) findViewById(R.id.buttonMachines);
+        buttonMachines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(HomeActivity.this, MachinesActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+        //do when button bmi pressed
+        buttonBMI = (Button) findViewById(R.id.buttonBMI);
+        buttonBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(HomeActivity.this, BMIActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+        //do when button edit workout pressed
+        buttonEditWorkout = (Button) findViewById(R.id.buttonEditWorkout);
+        buttonEditWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(HomeActivity.this, EditWorkoutActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+        //do when button workout pressed
+        buttonWorkout = (Button) findViewById(R.id.buttonWorkout);
+        buttonWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(HomeActivity.this, WorkoutActivity.class);
+                HomeActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
